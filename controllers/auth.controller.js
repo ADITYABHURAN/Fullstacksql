e382a0e9bd081d729af5999fa3ff5a15b32243bd
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-07-19 13:57:38 +0530

    worked on prisma and how to migrate db for prisma to neon

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,12 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 export const registerUser = async (req, res) => { 
    console.log("User registered");
-}
+   //this is how you register a user
+   //and also this is how you use prisma to interact with the database
+   await prisma.user.findUnique({
+      where: {email}
+   })
 
+};
---
+++ prisma/migrations/20250719081553_fullstacksql/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'user',
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "verificationToken" TEXT,
+    "passwordResetToken" TEXT,
+    "passwordResetExpiry" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
